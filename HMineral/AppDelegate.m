10c271c8c57124c79e01939c689570de78dfd9ef
irombie <iremlergun@gmail.com>
2016-01-23 15:03:21 +0200

    HMineral application-initial

--- HMineral/AppDelegate.m
+++ HMineral/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -17,6 +18,8 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    [Parse setApplicationId:@"c2HTape4T7VoMkJOYWmRHLegia3PTkzfNMn8ACxm"
+                  clientKey:@"UOGwukddfab9XNA3O9DXI3vCjqSnAe3JOnpgZnCP"];
     return YES;
 }
 
