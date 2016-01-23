e57a3a833e91254563a02a4dd8e4c6b94bd96889
irombie <iremlergun@gmail.com>
2016-01-23 14:55:00 +0200

    Initial Commit

---
+++ HMineral/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HMineral
+//
+//  Created by Gül Ergün on 23/01/16.
+//  Copyright © 2016 İrem Ergün. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HMineral/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HMineral
+//
+//  Created by Gül Ergün on 23/01/16.
+//  Copyright © 2016 İrem Ergün. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
